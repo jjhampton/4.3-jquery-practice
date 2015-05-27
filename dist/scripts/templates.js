47fb4c34c7165e5b1c32b818ad0c1f75442a8220
John Hampton <john_j_hampton@yahoo.com>
2015-05-27 17:44:37 -0400

    Create function renderApp and use w/ Handlebars templates to display directories Add additional CSS styling to directories to match original mock-up

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -1,4 +1,17 @@
 this["JST"] = this["JST"] || {};
 this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<h1>Hello</h1>\n";
+    return "<ul class=\"container\">\n</ul>\n";
+},"useData":true});
+this["JST"]["directories"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+    var stack1, helper, alias1=helpers.helperMissing, alias2="function";
+
+  return "  <li class=\"directory\">\n    <div class=\"directory-logo\">"
+    + ((stack1 = ((helper = (helper = helpers.icon || (depth0 != null ? depth0.icon : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"icon","hash":{},"data":data}) : helper))) != null ? stack1 : "")
+    + "<span>"
+    + this.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</span></div>\n    <div class=\"directory-value\"><p>COUNT<p></div>\n  </li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.menu : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,28 @@
 (function(){
   'use strict';
 
-  $.getJSON('http://localhost:9000/menu.json').then(logMenu);
+  $(document).ready(function() {
+    $.getJSON('http://localhost:9000/menu.json').then(app);
+  });
 
-  function logMenu(menu) {
+  function app(menu) {
     console.log(menu);
+    renderApp(menu);
   }
 
-  $(document).ready(function(){
+  function renderApp(menu) {
     $('body').prepend(JST['application']());
-  });
+    $('.container').prepend(JST['directories']({
+      menu: menu
+    }));
+  }
+
+  function getCountTotal(directory) {
+    var countTotal = 0; //initial sum value
+    _.each(directory.subdirectory, function(){
+      countTotal += subdirectory.count;
+    });
+  }
+
+
 })();
