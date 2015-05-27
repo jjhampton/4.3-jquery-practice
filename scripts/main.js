1de8fa7f50ad1ce4746a0b86f40a430c5d6c4b1f
John Hampton <john_j_hampton@yahoo.com>
2015-05-27 15:33:06 -0400

    Use $getJSON to get menu.json Log menu.json to console w/ function logMenu()

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 (function(){
   'use strict';
 
+  $.getJSON('http://localhost:9000/menu.json').then(logMenu);
+
+  function logMenu(menu) {
+    console.log(menu);
+  }
+
   $(document).ready(function(){
     $('body').prepend(JST['application']());
   });
