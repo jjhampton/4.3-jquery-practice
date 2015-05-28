11317b633dc9e199f7a8cd3010d373f215fa6b33
John Hampton <john_j_hampton@yahoo.com>
2015-05-27 23:59:25 -0400

    Style subdirectories w/ CSS Add function renderDirectory(), still needs refactoring to only render single

--- scripts/main.js
+++ scripts/main.js
@@ -8,31 +8,44 @@
   function app(menu) {
     console.log(menu);
     renderApp(menu);
+
   }
 
   function renderApp(menu) {
 
+    //Handlebars helper what will total up the number of individual items from a directory's subdirectories for use in displaying on the parent directory line on the top-level display
     Handlebars.registerHelper("getCountTotal", function(directory) {
       var countTotal = 0; //initial sum value
-      _.each(directory.subdirectories, function(element){
-        countTotal += element.count;
+      _.each(directory.subdirectories, function(subdirectory){
+        countTotal += subdirectory.count;
       });
       return countTotal;
+    });
+
+    //Handlebars helper to increment value by 1
+    Handlebars.registerHelper("incrementer", function(value) {
+      return parseInt(value) + 1;
     });
 
     $('body').prepend(JST['application']());
     $('.container').prepend(JST['directories']({
       menu: menu
     }));
+
+    _.each(menu, renderDirectory);
+
+    _.each(menu, function(directory) {
+      console.log(directory);
+    });
   }
 
-  // function getCountTotal(directory) {
-  //   var countTotal = 0; //initial sum value
-  //   _.each(directory.subdirectory, function(){
-  //     countTotal += subdirectory.count;
-  //   });
-  //   return countTotal;
-  // }
+
+
+  function renderDirectory(directory) {
+    $('.directory').after(JST['subdirectory']({
+      directory: directory
+    }));
+  }
 
 
 })();
